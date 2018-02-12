29a65db14884327e38d0799d69a583451c9d3cb9
taniabuzykina <36134034+taniabuzykina@users.noreply.github.com>
2018-02-12 21:08:05 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let translate = (text) => {
+    let transFrom = document.querySelector(".js-translate-from").value;
+    let transTo = document.querySelector(".js-translate-to").value;
+    let transOption = transFrom+"-"+transTo;
     let xhr = new XMLHttpRequest();
 
     const key = 'trnsl.1.1.20180212T170957Z.610dcf1db261e404.5f0ef5f921dd000987d5e71976c4dddb9a049940';
 
     let body = `key=${encodeURIComponent(key)}
                 &text=${encodeURIComponent(text)}
-                &lang=${encodeURIComponent('en-de')}`;
+                &lang=${encodeURIComponent(transOption)}`;
 
     xhr.open('POST', 'https://translate.yandex.net/api/v1.5/tr.json/translate?', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
