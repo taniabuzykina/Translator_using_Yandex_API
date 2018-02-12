26751b3bdee99b868cff93b1c57f4b1c85dc3d28
taniabuzykina <36134034+taniabuzykina@users.noreply.github.com>
2018-02-12 18:56:50 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let translate = (text) => {
     let xhr = new XMLHttpRequest();
-    const key = 'trnsl.1.1.20180212T144108Z.a4abf848e52820a3.eee565c1f2668137e318bdd1d4e3787bfc0b3ba8';
+    const key = 'trnsl.1.1.20180212T152351Z.2753a157dc3c5ab3.084990b177e396db4c2e8ee9c4146a87e0d03e0a';
     let body = `key=${encodeURIComponent(key)}
                 &text=${encodeURIComponent(text)}
                 &lang=ru`;
