e9ec3871fd12edce8db47110e987ee0c0a23b059
taniabuzykina <36134034+taniabuzykina@users.noreply.github.com>
2018-02-12 20:40:59 +0200

    update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let translate = (text) => {
     const key = 'trnsl.1.1.20180212T170957Z.610dcf1db261e404.5f0ef5f921dd000987d5e71976c4dddb9a049940';
 
     let body = `key=${encodeURIComponent(key)}
-                &text=${encodeURIComponent('Hello world')}
+                &text=${encodeURIComponent(text)}
                 &lang=${encodeURIComponent('en-ru')}`;
 
     xhr.open('POST', 'https://translate.yandex.net/api/v1.5/tr.json/translate?', true);
@@ -13,12 +13,15 @@ let translate = (text) => {
     xhr.onload = function () {
         if (this.status == 200) {
             let result = JSON.parse(this.responseText);
-
+            let translate = result.text;
             console.log(this.responseText);
+            let resText = document.querySelector(".js-result");
+            resText.innerHTML = translate;
         } else console.log('There was an error');
     };
 
     xhr.send(body);
 };
 
-translate('hello from the other side');
+
+translate('my songs know what you did in the dark');
