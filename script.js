3bea91f70d657596275c4a789dd97c45a8fcbf70
taniabuzykina <36134034+taniabuzykina@users.noreply.github.com>
2018-02-12 20:02:33 +0200

    update

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let translate = (text) => {
     let xhr = new XMLHttpRequest();
-    const key = 'trnsl.1.1.20180212T152351Z.2753a157dc3c5ab3.084990b177e396db4c2e8ee9c4146a87e0d03e0a';
-    let body = `key=${encodeURIComponent(key)}
-                &text=${encodeURIComponent(text)}
-                &lang=ru`;
 
-    xhr.open('GET', 'https://translate.yandex.net', true);
+    const key = 'trnsl.1.1.20180212T170957Z.610dcf1db261e404.5f0ef5f921dd000987d5e71976c4dddb9a049940';
+
+    let body = `key=${encodeURIComponent(key)}
+                &text=${encodeURIComponent('Hello world')}
+                &lang=${encodeURIComponent('en-ru')}`;
+
+    xhr.open('POST', 'https://translate.yandex.net/api/v1.5/tr.json/translate?', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-    xhr.send(body);
-
-    xhr.onload = () => {
+    xhr.onload = function () {
         if (this.status == 200) {
-            let response = JSON.parse(this.responseText);
+            let result = JSON.parse(this.responseText);
 
-            console.log("OK");
-        }
+            console.log(this.responseText);
+        } else console.log('There was an error');
     };
+
+    xhr.send(body);
 };
 
 translate('hello from the other side');
